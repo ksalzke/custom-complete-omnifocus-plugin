744b41fe6e786928a7515e7ca8b43ac6cd95a502
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-03-04 21:32:06 +1100

    feat: :sparkles: add 'preferences' pane and convert 'tags to remove' to synced pref
    BREAKING CHANGE: 'tags to remove' are now set in preferences pane rather than in config file; Synced Preferences plug-in is required; completion function changed to 'onComplete'

---
+++ CustomComplete.omnifocusjs/Resources/preferences.js
@@ -0,0 +1,23 @@
+/* global PlugIn Form flattenedTags */
+(() => {
+  const action = new PlugIn.Action(async function (selection, sender) {
+    const syncedPrefs = this.customCompleteLib.loadSyncedPrefs()
+
+    // get current preferences or set defaults if they don't yet exist
+    const tagsToRemove = this.customCompleteLib.tagsToRemove()
+
+    // create and show form
+    const form = new Form()
+    form.addField(new Form.Field.MultipleOptions('tagsToRemove', 'Tag(s) to remove when a task is completed', flattenedTags, flattenedTags.map(t => t.name), tagsToRemove))
+    await form.show('Preferences: Custom Complete', 'OK')
+
+    // save preferences
+    syncedPrefs.write('tagsToRemoveIDs', form.values.tagsToRemove.map(tag => tag.id.primaryKey))
+  })
+
+  action.validate = function (selection, sender) {
+    return true
+  }
+
+  return action
+})()
